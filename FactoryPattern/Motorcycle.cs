e141a4f13b951bf7ed1417ee1eb402abbda7933a
Jake Johnston <jakejohnston618@gmail.com>
2023-03-27 09:44:49 -0500

    created vehicle factory

---
+++ FactoryPattern/Motorcycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class Motorcycle : IVehicle
+    {
+        public Motorcycle() 
+        {
+            Console.WriteLine("Building a new motorcylce.");
+        }
+        public void Drive()
+        {
+            Console.WriteLine($"{GetType().Name} is now driving.");
+        }
+    }
+}
--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("How many wheels does your vehicle have?");
+            var numberOfWheels = Console.ReadLine();
+
+            IVehicle vehicle = VehicleFactory.GetVehicle(numberOfWheels);
+
+            Console.WriteLine();
+            vehicle.Drive();
         }
     }
 }
